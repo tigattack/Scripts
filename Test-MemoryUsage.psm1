cd44f938a8e71a7ab2259e81e55ac51198ae5333
tigattack <10629864+tigattack@users.noreply.github.com>
2021-03-04 14:09:35 +0000

    feat(Test-MemoryUsage): Create.

---
+++ Test-MemoryUsage.psm1
@@ -0,0 +1,27 @@
+# Example output:
+# Hostname Status PctFree FreeGB TotalGB
+# -------- ------ ------- ------ -------
+# FS02	   OK        55.8  428.4     768
+
+Function Test-MemoryUsage {
+    [cmdletbinding()]
+
+    $os = Get-Ciminstance Win32_OperatingSystem
+    $pctFree = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100,2)
+
+    if ($pctFree -ge 45) {
+    $Status = "OK"
+    }
+    elseif ($pctFree -ge 15 ) {
+    $Status = "Warning"
+    }
+    else {
+    $Status = "Critical"
+    }
+
+    $os | Select @{Name = "Hostname";Expression = {hostname}},
+    @{Name = "Status";Expression = {$Status}},
+    @{Name = "% Free"; Expression = {$pctFree}},
+    @{Name = "FreeGB";Expression = {[math]::Round($_.FreePhysicalMemory/1mb,2)}},
+    @{Name = "TotalGB";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}} | Format-Table -AutoSize
+}
