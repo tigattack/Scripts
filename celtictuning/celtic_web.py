408cde5859b371a5d86ccba202077aca325e05ff
tigattack <10629864+tigattack@users.noreply.github.com>
2023-02-13 15:17:19 +0000

    celtic_web: query JSON data, return as tables
    Also various other changes to improve styling, add more info, and support changes made in the Celtic class in 6edf3cc

--- celtictuning/celtic_web.py
+++ celtictuning/celtic_web.py
@@ -2,44 +2,32 @@
 import os
 
 from flask import Flask, render_template, request
-from markupsafe import Markup
+from flask.wrappers import Response
 
 from . import Celtic
 
 template_dir = os.path.abspath('./web_templates')
 app = Flask(__name__, template_folder=template_dir)
 
-def vehicle_info(vrn: str) -> str:
+def vehicle_info(vrn: str):
     """Just a silly little wrapper for my Celtic class"""
     try:
         celtic = Celtic.Celtic(vrn)
     except ValueError as err:
-        return str(err)
+        return Response(
+            str(err),
+            status=400,
+        )
 
-    return celtic.get_all_pretty()
-
-# TODO:just get all data as JSON and sort into tables rather than dicking around with hacky spacing
+    return celtic.get_all()
 
 @app.route('/')
-def interactive_request():
+def index():
     """
     Function for web interface.
     'Interface' is a bit of a stretch but just go with it.
     """
     return render_template('page.html')
-
-
-@app.route('/get_vehicle_pretty')
-def get_pretty_data():
-    """Return prettified vehicle data"""
-
-    if not request.args.get('vrn'):
-        return 'ERROR: Please provide a vehicle registration.'
-
-    vrn = request.args.get('vrn', '')
-    if len(vrn) == 0:
-        return 'Please provide a vehicle registration.'
-    return vehicle_info(vrn)
 
 
 @app.route('/get_vehicle')
